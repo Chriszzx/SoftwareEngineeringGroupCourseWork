c98a9ce8fcbd1e7e59d1e14de84867bfdb0818af
AntiAnimus <506326634@qq.com>
2018-12-08 15:33:56 +0000

    new

--- trafficmon/test/com.trafficmon/CongestionChargeSystemTest.java
+++ trafficmon/test/com.trafficmon/CongestionChargeSystemTest.java
@@ -1,7 +1,6 @@
 package com.trafficmon;
 
 import org.junit.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertTrue;
