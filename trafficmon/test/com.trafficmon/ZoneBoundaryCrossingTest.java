b24cedbeccaf3288220d8c46f0f07ba388515226
YankeZhang <32335582+YankeZhang@users.noreply.github.com>
2018-12-09 14:00:49 +0000

    Add files via upload

---
+++ trafficmon/test/com.trafficmon/ZoneBoundaryCrossingTest.java
@@ -0,0 +1,29 @@
+package com.trafficmon;
+
+import com.trafficmon.Vehicle;
+import com.trafficmon.ZoneBoundaryCrossing;
+import org.junit.Test;
+import org.joda.time.Hours;
+import org.joda.time.LocalTime;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ZoneBoundaryCrossingTest {
+
+    LocalTime local = new LocalTime();
+    @Test
+    public void getVehicleTest(){
+        ZoneBoundaryCrossing crossing = new ZoneBoundaryCrossing(Vehicle.withRegistration("AA")) {};
+        assertTrue(crossing.getVehicle().equals(Vehicle.withRegistration("AA")));
+    }
+
+    @Test
+    public void timestampTest()
+    {
+        ZoneBoundaryCrossing crossing = new ZoneBoundaryCrossing(Vehicle.withRegistration("AA")) {};
+        assertEquals(crossing.timestamp(),local.getHourOfDay()*60+local.getMinuteOfHour());
+    }
+
+}
